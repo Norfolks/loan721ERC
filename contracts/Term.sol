330d806e99fc6e494d00cfee1c57f0cb0af54355
Mykola Ivanov <norfolknorfolks@gmail.com>
2018-03-24 17:11:45 +0200

    add dharmaprotocol contracts

---
+++ contracts/Term.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.0;
+
+import "zeppelin-solidity/contracts/math/SafeMath.sol";
+
+
+contract Term {
+    function Term(){
+
+    }
+}
---
+++ migrations/2_tetms.js
@@ -0,0 +1,5 @@
+var Term = artifacts.require("./Term.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(Term);
+};
